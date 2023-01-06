4daf5ee355cf4c2a005bc0bb1ddb995a9a6fd389
Seiya830 <seiya@MacBook-Pro.local>
2023-01-06 14:56:05 +0900

    ロジック部分完成

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,31 +1,46 @@
 <script lang="ts">
-import { defineComponent } from 'vue'
-import type { PropType } from 'vue'
-
-export enum Hand { Guu = 0, Choki, Paa}
-export type ActionPonType = (Hand: number) => void
+import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
-  props: {
-    actionPon: {
-      type: Function as PropType<ActionPonType>,
-      required: true
+  name: 'Game',
+  setup() {
+    const result = ref('');
+    const computer = ref('');
+
+    function play(player: string) {
+       // コンピュータの手をランダムに決める
+      const hands = ['グー', 'チョキ', 'パー'];
+      computer.value = hands[Math.floor(Math.random() * hands.length)];
+
+      // じゃんけんの勝敗を判定するロジック
+      if (player === computer.value) {
+        result.value = '引き分け';
+      } else if (
+        (player === 'グー' && computer.value === 'チョキ') ||
+        (player === 'チョキ' && computer.value === 'パー') ||
+        (player === 'パー' && computer.value === 'グー')
+      ) {
+        result.value = '勝ち';
+      } else {
+        result.value = '負け';
+      }
     }
+
+    return {
+      result,
+      computer,
+      play,
+    };
   },
-  setup () {
-    return { Hand }
-  }
-})
+});
 </script>
 
 <template>
   <div>
-    <button v-on:click="actionPon(Hand.Guu)">グー</button>
-    <button v-on:click="actionPon(Hand.Choki)">チョキ</button>
-    <button v-on:click="actionPon(Hand.Paa)">パー</button>
+    <p>{{ result }}</p>
+    <p>相手： {{ computer }}</p>
+    <button @click="play('グー')">グー</button>
+    <button @click="play('チョキ')">チョキ</button>
+    <button @click="play('パー')">パー</button>
   </div>
 </template>
-
-<style>
-
-</style>
