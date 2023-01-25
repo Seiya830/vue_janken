02e77a436a3239cbb49541eb52d0e03c73fa9905
Seiya830 <seiya@MacBook-Pro.local>
2023-01-26 00:25:43 +0900

    パスの追加

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'vue_janken',
+  base: '/vue_janken',
   plugins: [vue()],
   resolve: {
     alias: {
